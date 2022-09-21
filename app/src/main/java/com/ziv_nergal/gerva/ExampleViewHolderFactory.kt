b5b55b029b0b53bf866e200ef2615e6f73a51424
Ziv Nergal <ziv32155@gmail.com>
2022-09-22 00:53:39 +0300

    Refactoring

--- app/src/main/java/com/ziv_nergal/gerva/ExampleViewHolderFactory.kt
+++ app/src/main/java/com/ziv_nergal/gerva/ExampleViewHolderFactory.kt
@@ -2,9 +2,9 @@ package com.ziv_nergal.gerva
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import com.ziv_nergal.genericrecyclerviewadapter.GenericRecyclerViewAdapter
 import com.ziv_nergal.genericrecyclerviewadapter.ViewHolderFactory
+import com.ziv_nergal.gerva.databinding.ItemCardViewBinding
 
 class ExampleViewHolderFactory : ViewHolderFactory {
 
@@ -16,9 +16,8 @@ class ExampleViewHolderFactory : ViewHolderFactory {
         return when (viewType) {
             R.layout.item_card_view ->
                 CardViewHolder(
-                    DataBindingUtil.inflate(
+                    ItemCardViewBinding.inflate(
                         LayoutInflater.from(parent.context),
-                        viewType,
                         parent,
                         false
                     )
